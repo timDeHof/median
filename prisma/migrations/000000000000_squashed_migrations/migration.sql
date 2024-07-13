68f930af40e4755daa0e549f1d4a03182d835032
timDeHof <ttdehof@gmail.com>
2024-07-13 10:14:59 -0400

    chore(migrations): squash migrations

--- prisma/migrations/000000000000_squashed_migrations/migration.sql
+++ prisma/migrations/000000000000_squashed_migrations/migration.sql
@@ -1,5 +1,16 @@
--- AlterTable
-ALTER TABLE "Article" ADD COLUMN     "authorId" INTEGER;
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "body" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" INTEGER,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -14,7 +25,11 @@ CREATE TABLE "User" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Article_title_key" ON "Article"("title");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
